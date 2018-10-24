432b039f2000f5bdc97395ed7133919723231faa
Thomas <tjb295@nau.edu>
2018-10-24 14:22:32 -0700

    Add project files.

---
+++ HolidayGroupie/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HolidayGroupie.MvcApplication" Language="C#" %>
---
+++ HolidayGroupie/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HolidayGroupie.Startup))]
+namespace HolidayGroupie
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
